610eae8762c20f0f126af1e157a73d136acc6da1
harriett-gl <144355411+harriett-gl@users.noreply.github.com>
2023-09-12 08:13:35 -0600

    Update Ejercicio01.cpp

--- PA_T_Parcial_01/Ejercicio01.cpp
+++ PA_T_Parcial_01/Ejercicio01.cpp
@@ -1,4 +1,6 @@
 #include "Ejercicio01.h"
+#include <iostream>
+#include <cctype> 
 
 char Ejercicio01::lower(char c)
 {
@@ -10,5 +12,33 @@ char Ejercicio01::lower(char c)
 
 bool Ejercicio01::isPalindrome(const char* phrase, int length)
 {
-    return false;
+    int derecha = 0;
+    int izquiera = length - 1;
+
+    while (derecha < izquierda)
+    {
+        char derechaChar = std::tolower(phrase[derecha]);
+        char izquierdaChar = std::tolower(phrase[izquierda]);
+
+        if (!isalnum(derechaChar))
+        {
+            left+1;
+            continue;
+        }
+        if (!isalnum(izquierdaChar))
+        {
+            right-1;
+            continue;
+        }
+
+        if (derechaChar != derechaChar)
+        {
+            return false;
+        }
+
+        derecha+1;
+        izquiera-1;
+    }
+
+    return true;
 }
