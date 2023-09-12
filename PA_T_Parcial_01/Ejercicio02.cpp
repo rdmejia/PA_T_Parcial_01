f9e3db827bc35d4ccf3bd8d0960d3215999fab0a
harriett-gl <144355411+harriett-gl@users.noreply.github.com>
2023-09-12 08:31:43 -0600

    Update Ejercicio02.cpp

--- PA_T_Parcial_01/Ejercicio02.cpp
+++ PA_T_Parcial_01/Ejercicio02.cpp
@@ -4,5 +4,12 @@ int Ejercicio02::rob(int houses[], int n)
 {
     int houses[] = { 2, 7, 9, 3, 1 };
 	int maxMoney = Ejercicio02::rob(houses, sizeof(houses) / sizeof(houses[0]));
+	int dp[];
+
+for (int i = 2; i < n; i++)
+{
+	dp[0] = houses[0];
+}
+return 0;
     return 0;
 }
