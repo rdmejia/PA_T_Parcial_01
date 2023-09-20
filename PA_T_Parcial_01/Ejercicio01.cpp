3398e7654091d951b1c9a8f195508dbf4d422b50
Daniel Mejia <rdmejiaa@gmail.com>
2023-09-19 18:51:21 -0600

    Solve ejercicio01

--- PA_T_Parcial_01/Ejercicio01.cpp
+++ PA_T_Parcial_01/Ejercicio01.cpp
@@ -1,5 +1,9 @@
 #include "Ejercicio01.h"
 
+bool isAlphanumeric(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z');
+}
+
 char Ejercicio01::lower(char c)
 {
     if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
@@ -10,5 +14,38 @@ char Ejercicio01::lower(char c)
 
 bool Ejercicio01::isPalindrome(const char* phrase, int length)
 {
-    return false;
+    // Caso base: todas las frases/palabras de un caracter o menos son palindromos
+    if (length < 2)
+        return true;
+
+    /* 
+    *phrase == phrase[0] 
+    Acortamos la frase, incrementando el apuntador de phrase y decrementando la longitud
+    hasta que la frase no empiece con un caracter que no sea alphanumerico
+    */
+
+    while (!isAlphanumeric(*phrase) && length > 1) {
+        ++phrase;
+        --length;
+    }
+
+    /*
+    Acortamos la frase si el ultimo caracter no es alfanumerico
+    Si el ultimo caracter no es alfanumerico, entonces solo debemos restarle a la longitud
+    */
+
+    while (!isAlphanumeric(*(phrase + length - 1)) && length > 1) {
+        --length;
+    }
+
+    /*
+    Comparamos que el caracter de inicio de la frase sea
+    que su ultimo caracter
+    */
+
+    if (lower(*phrase) != lower(*(phrase + length - 1)))
+        return false;
+
+    // El primer y ultimo caracter si son iguales, pasemos a comparar el segundo y el penultimo
+    return isPalindrome(phrase + 1, length - 2);
 }
